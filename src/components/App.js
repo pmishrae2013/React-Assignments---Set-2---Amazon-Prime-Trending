ed8e6512c29ff99634a7c00b6901111714625fe6
Purushottam <85189176+pmishrae2013@users.noreply.github.com>
2021-07-23 14:53:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,41 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <></>
+//   )
+// }
+
+
+// export default App;
+
+const App = (props) => {
+  const[slide, setSlide] = useState(props.slides[0]);
+  const[slideNumber,setSlideNumber] = useState(0);
+
+  function next(){
+    setSlideNumber(slideNumber+1);
+    setSlide(props.slides[slideNumber])
+  }
+
+  function prev(){
+    setSlideNumber(slideNumber-1);
+    setSlide(props.slides[slideNumber])
+  }
+
+  function restart(){
+    setSlideNumber(0)
+  }
+  
   return (
-    <></>
+    <>
+      <h1>{props.slides.title}</h1>
+      <p>{props.slides.text}</p>
+      <button data-testid="button-next" disabled={slideNumber===props.slides.length-1? true:false} onClick={next}>Next</button>
+      <button data-testid="button-prev" disabled={slideNumber===0? true:false} onClick={prev}>Prev</button>
+      <button data-testid="button-restart" onClick={restart}>Restart</button>
+    </>
   )
 }
 
