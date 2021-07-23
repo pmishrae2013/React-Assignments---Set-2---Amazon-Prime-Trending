fd419e47394e738db20be3be626f82d46cd101f9
Purushottam <85189176+pmishrae2013@users.noreply.github.com>
2021-07-23 16:11:26 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -11,18 +11,18 @@ import slides from '../data';
 
 // export default App;
 
-const App = (props) => {
-  const[slide, setSlide] = useState(props.slides[0]);
+const App = (slides) => {
+  const[slide, setSlide] = useState(slides[0]);
   const[slideNumber,setSlideNumber] = useState(0);
 
   function next(){
     setSlideNumber(slideNumber+1);
-    setSlide(props.slides[slideNumber])
+    setSlide(slides[slideNumber])
   }
 
   function prev(){
     setSlideNumber(slideNumber-1);
-    setSlide(props.slides[slideNumber])
+    setSlide(slides[slideNumber])
   }
 
   function restart(){
@@ -31,9 +31,9 @@ const App = (props) => {
   
   return (
     <>
-      <h1 data-testid="title">{props.slides.title}</h1>
-      <p data-testid="text">{props.slides.text}</p>
-      <button data-testid="button-next" disabled={slideNumber===props.slides.length-1? true:false} onClick={next}>Next</button>
+      <h1 data-testid="title">{slide.title}</h1>
+      <p data-testid="text">{slide.text}</p>
+      <button data-testid="button-next" disabled={slideNumber===slides.length-1? true:false} onClick={next}>Next</button>
       <button data-testid="button-prev" disabled={slideNumber===0? true:false} onClick={prev}>Prev</button>
       <button data-testid="button-restart" disabled={slideNumber===0? true:false} onClick={restart}>Restart</button>
     </>
